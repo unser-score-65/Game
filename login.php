7e243c2dc72296afc9df0aa24e5d81347030b076
KSHIT RAJ SRIVASTAVA <61612598+KSHIT1899@users.noreply.github.com>
2020-10-01 00:04:06 +0530

    changed some allignment

--- login.php
+++ login.php
@@ -18,7 +18,13 @@ if(count($_POST)>0) {
 </head>
 <body>
 <form name="frmUser" method="post" action="">
-	<div class="message"><?php if($message!="") { echo $message; } ?></div>
+    <div class="message">
+    <?php if($message!="") 
+    { 
+        echo $message; 
+    } 
+    ?>
+   </div>
 		<table border="0" cellpadding="10" cellspacing="1" width="500" align="center" class="tblLogin">
 			<tr class="tableheader">
 			<td align="center" colspan="2">Enter Login Details</td>
@@ -36,4 +42,5 @@ if(count($_POST)>0) {
 			</tr>
 		</table>
 </form>
-</body></html>
+</body>
+</html>
